b05fda7aef67db7b34f5bb7fb945dd87b0321ccb
Mateusz Mazurek <mateusz.mazurek@explaineverything.com>
2018-02-20 12:32:20 +0100

    [feature/yarn-support] yarn default dependency manager

---
+++ cacheDependencyManagers/yarnConfig.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var path = require('path');
+var shell = require('shelljs');
+var fs = require('fs');
+var md5 = require('md5');
+
+function getFileHash(filePath) {
+  var json = JSON.parse(fs.readFileSync(filePath));
+  return md5(JSON.stringify({
+    dependencies: json.dependencies,
+    devDependencies: json.devDependencies
+  }));
+}
+
+module.exports = {
+  cliName: 'yarn',
+  getCliVersion: function getYarnVersion () {
+    return shell.exec('yarn --version', {silent: true}).output.trim();
+  },
+  configPath: path.resolve(process.cwd(), 'package.json'),
+  installPath: 'node_modules',
+  installCommand: 'yarn install',
+  getFileHash: getFileHash
+};
